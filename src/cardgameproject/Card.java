99f2d370c7ec5322a97ec6b37d51304f9c8ec5cc
Zydia <zydia@sheridan.desire2learn.com. on. ca>
2021-06-17 21:59:34 -0400

    first commit

---
+++ src/cardgameproject/Card.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardgameproject;
+
+/**
+ * A class to be used as the base Card class for the project. Must be general
+ * enough to be instantiated for any Card game. Students wishing to add to the code 
+ * should remember to add themselves as a modifier.
+ * @author dancye, 2018
+ */
+public abstract class Card 
+{
+    //default modifier for child classes
+    
+    /**
+     * Students should implement this method for their specific children classes 
+     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     */
+    
+    @Override
+    public abstract String toString();
+    
+}
